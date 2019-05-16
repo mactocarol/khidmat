4df627586e5f48743d3119af3ba0b070b79e6b53
Parvez Khan <mss.parvezkhan@gmail.com>
2019-05-16 12:44:04 +0530

    set base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://kabayanzone.ae/carol/khidmat';
+$config['base_url'] = 'http://localhost/khidmat/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://kabayanzone.ae/carol/khidmat';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
